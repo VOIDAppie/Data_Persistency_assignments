30b4ebf99c25dc776b70503b5527b6cfd96a73c7
ALPEREN ELCIK <alperen.elcik@hva.nl>
2022-09-09 15:11:35 +0200

    opdracht 1 versie 1 af

---
+++ src/main/java/Reiziger.java
@@ -0,0 +1,59 @@
+import java.util.Date;
+
+public class Reiziger {
+    private int reizigerId;
+    private String voorletters;
+    private String tussenvoegsel;
+    private String achternaam;
+
+
+    public Reiziger(int reizigerId, String voorletters, String tussenvoegsel, String achternaam) {
+        this.reizigerId = reizigerId;
+        this.voorletters = voorletters;
+        this.tussenvoegsel = tussenvoegsel;
+        this.achternaam = achternaam;
+
+    }
+
+    public int getReizigerId() {
+        return reizigerId;
+    }
+
+    public void setReizigerId(int reizigerId) {
+        this.reizigerId = reizigerId;
+    }
+
+    public String getVoorletters() {
+        return voorletters;
+    }
+
+    public void setVoorletters(String voorletters) {
+        this.voorletters = voorletters;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    @Override
+    public String toString() {
+        return "Reiziger{" +
+                "reizigerId=" + reizigerId +
+                ", voorletters='" + voorletters + '\'' +
+                ", tussenvoegsel='" + tussenvoegsel + '\'' +
+                ", achternaam='" + achternaam + '\'' +
+                '}';
+    }
+}
